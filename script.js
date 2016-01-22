6f980db96ba28281caa019592f422625f8f7306b
Milan Grubnić <MilanG@aol.com>
2016-01-21 21:38:47 -0600

    added click event handler to count up or down clicks

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function init() {
+    var upVote = 0;
+    var downVote = 0;
+
+    $('#up').click(function() {
+        upVote += 1;
+        console.log(upVote);
+    })
+
+    $('#down').click(function() {
+        downVote += 1;
+        console.log(downVote);
+    })
+
+}
+
 $(document).ready(function() {
-    alert('jQuery Linked!');
+    init();
 });
